de10cf5b3a7b2a99cd24b193130a019c84a13a37
RubyNguyen07 <rubynguyen2003@gmail.com>
2024-11-04 16:01:03 +0800

    MVP

---
+++ inventory/backend/init_db.py
@@ -0,0 +1,47 @@
+""" init_db.py file """
+import numpy as np
+import pandas as pd
+import psycopg2
+from io import StringIO
+
+from sklearn.model_selection import train_test_split
+from db import db_engine 
+
+create_table_sql = """
+CREATE TABLE IF NOT EXISTS inventory (
+    id serial PRIMARY KEY,
+    startOfWeek DATE NOT NULL UNIQUE,
+    chickenOrder INTEGER DEFAULT 0,
+    fishOrder INTEGER DEFAULT 0,
+    saladOrder INTEGER DEFAULT 0
+);
+"""
+
+get_data_sql = """select * from inventory order by startOfWeek ASC"""
+
+df = pd.read_csv("data/pastInventory.csv", index_col="id")
+test_size= 0.20 # 20% for testing
+df_train, df_test = train_test_split(df, test_size=test_size, random_state=1234)
+df_train.to_csv("data/train.csv", index=False)
+df_test.to_csv("data/test.csv", index=False)
+df_test = df_test.copy()
+
+df = pd.concat([df_train, df_test])
+
+try:
+    conn = psycopg2.connect(db_engine())
+    cur = conn.cursor()
+    print("Create inventory table")
+    cur.execute(create_table_sql)
+    conn.commit()
+    print("Insert train and test data into table ...")
+    buffer = StringIO()
+    df.to_csv(buffer, index_label="id", header=False)
+    buffer.seek(0)
+    cur.copy_from(buffer, "inventory", sep=",")
+    conn.commit()
+    print("Insert finished.")
+    print(df)
+    cur.close()
+except Exception as e:
+    print("Problems:", str(e))
